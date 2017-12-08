9a40f304eebdcc73ecd14258b5ca97f854f78855
João Almeida <joao.diogo.almeida@gmail.com>
2017-12-08 22:01:32 +0000

    Azure Table Bot state in Node

--- src/generators/bbv3-node/templates/src/bot.js
+++ src/generators/bbv3-node/templates/src/bot.js
@@ -1,11 +1,15 @@
 var builder = require('botbuilder');
+var botAzure = require('botbuilder-azure');
 var telemetryModule = require('./util/telemetry-module.js');
 
 telemetryModule.Initialize();
 
 function create(connector) {
 
-    var bot = new builder.UniversalBot((connector));
+   var azureTableClient = new botAzure.AzureTableClient('storage', process.env.STATE_STORAGE_CONNECTION_STRING);
+   var tableStorage = new botAzure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+    var bot = new builder.UniversalBot(connector).set('storage', tableStorage);;
 
     bot.dialog('/', require('./dialogs/root'));
     //location.create(bot);
